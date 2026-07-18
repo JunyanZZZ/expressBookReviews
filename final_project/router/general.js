b0ebbf0adcb4d8e93496ecf2f2a27e260ce4d1ab
Junyan <junyan.zhang91@gmail.com>
2026-07-18 14:53:53 -0400

    finished task2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,14 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn);
+  const totalBook = Object.values(books).length;
+  if (isbn > 0 && isbn <= totalBook) {
+    res.send(books[isbn]);
+  }
+  else {
+    res.send('invalid isbn');
+  }
  });
   
 // Get book details based on author
