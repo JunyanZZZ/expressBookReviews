383da934865bc32bb703e400fa4093beeb87146c
Junyan <junyan.zhang91@gmail.com>
2026-07-18 16:59:44 -0400

    finish task7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,13 +10,43 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+
+    if (validusers.length > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if username or password missing
+  if (!username || !password) {
+    return res.send('Error login in: You need to give both username and password');
+  }
+
+  if (authenticatedUser(username, password)) {
+    // generate JWT access token
+    let accessToken = jwt.sign(
+        {data: password}, 'access', {expiresIn: 60 * 60}
+    );
+
+    // store access token in session
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.send("User successfully logged in");
+  }
+  else {
+    return res.send("Invalid Login. Check username and password");
+  }
 });
 
 // Add a book review
